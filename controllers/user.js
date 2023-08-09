fb22c8b3e1bbf7f0904fc8b870c10c07f7e333bd
Payal Mittal <payal8949531232@gmail.com>
2023-08-10 03:00:26 +0530

    change

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,9 @@ export const myProfile=(req,res,next)=>{
      res.status(200).json({
         success:true,
         user:req.user,
-     });       
+       
+     });  
+         
 };
 export const logout=(req,res,next)=>{
    req.session.destroy((err)=>{
--- routes/user.js
+++ routes/user.js
@@ -9,6 +9,7 @@ router.get("/googlelogin",passport.authenticate("google", {scope: ["profile"],})
 
 
 router.get("/login",passport.authenticate("google",{
+  
     successRedirect: process.env.FRONTEND_URL,
 }));
 
