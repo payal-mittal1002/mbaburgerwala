bb907d2c019bd72889c8d0e4c158026593437f8f
Payal Mittal <payal8949531232@gmail.com>
2023-08-10 23:29:02 +0530

    change

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,8 @@ router.get("/googlelogin",passport.authenticate("google", {scope: ["profile"],})
 
 
 router.get("/login",passport.authenticate("google",{
-  
-    successRedirect: process.env.FRONTEND_URL,
+    
+    // successRedirect: process.env.FRONTEND_URL,
 }));
 
 router.get("/me",isAuthenticated,myProfile);
